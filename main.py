1b3a7321f7ec14355610d29fb4648bdb3c6b6072
maius02 <maiapaez02@gmail.com>
2025-04-04 17:41:54 -0300

    separo mi codigo fuente en la carpeta src

--- main.py
+++ main.py
@@ -1,46 +1,6 @@
 #este programa limpia el dataset de espacios, cosas mal escritas y demas
 #el objetivo es que los datos esten uniformes
 
-def clean_name(name):
-    """Si el nombre tiene valor, limpia los espacios en blanco    """
-    return name.strip().capitalize() if name else None
-
-def clean_age(age):
-    """si la edad tiene un valor, devuelve la edad en entero"""
-    if not age or age.lower() in ["n/a","null","name",""]:
-        return None
-    
-    if age.isnumeric():
-        return int(age)
-    else:
-        return None
-
-def clean_gmail(gmail):
-    """si el email tiene valor, lo devuelve en minuscula, ademas debe tener el arroba"""
-    if gmail is None:
-        return None
-
-    gmail = gmail.lower().strip()
-    return gmail if "@" in gmail else None
-
-def clean_country(country):
-    """si el pais tiene valor, lo devuelve en mayuscula""" 
-    if country is None:
-        return None
-    
-    clean_country = country.strip().capitalize()
-    return clean_country if clean_country in ["Argentina","Brasil","Chile"] else "Resto del mundo"
-
-def clean_data(dataset):
-    """Limpia los datos del dataset"""
-    for data in dataset:
-        data["name"] = clean_name(data["name"])
-        data["age"] = clean_age(data["age"])
-        data["gmail"] = clean_gmail(data["gmail"])
-        data["country"] = clean_country(data["country"])
-    return dataset
-
-
 dataset = [
     {
         "name": "Ana Torres      ", 
@@ -80,7 +40,8 @@ dataset = [
     },
 ]
 
-#EJECUCION DEL PROGRAMA
+#from cleaner import clean_data
+from src.cleaner import clean_data
 print(clean_data(dataset))
 
 
---
+++ src/cleaner.py
@@ -0,0 +1,39 @@
+
+def clean_name(name):
+    """Si el nombre tiene valor, limpia los espacios en blanco    """
+    return name.strip().capitalize() if name else None
+
+def clean_age(age):
+    """si la edad tiene un valor, devuelve la edad en entero"""
+    if not age or age.lower() in ["n/a","null","name",""]:
+        return None
+    
+    if age.isnumeric():
+        return int(age)
+    else:
+        return None
+
+def clean_gmail(gmail):
+    """si el email tiene valor, lo devuelve en minuscula, ademas debe tener el arroba"""
+    if gmail is None:
+        return None
+
+    gmail = gmail.lower().strip()
+    return gmail if "@" in gmail else None
+
+def clean_country(country):
+    """si el pais tiene valor, lo devuelve en mayuscula""" 
+    if country is None:
+        return None
+    
+    clean_country = country.strip().capitalize()
+    return clean_country if clean_country in ["Argentina","Brasil","Chile"] else "Resto del mundo"
+
+def clean_data(dataset):
+    """Limpia los datos del dataset"""
+    for data in dataset:
+        data["name"] = clean_name(data["name"])
+        data["age"] = clean_age(data["age"])
+        data["gmail"] = clean_gmail(data["gmail"])
+        data["country"] = clean_country(data["country"])
+    return dataset
